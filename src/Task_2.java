b0d1af452ff62e63e0539d885e116e1ae9ab1eb6
Denis <ivdenvas@gmail.com>
2023-08-04 10:19:18 +0300

    task2

---
+++ src/Task_2.java
@@ -0,0 +1,13 @@
+public class Task_2 {
+    public static void main(String[] args) {
+        try {
+            int d = 0;
+            int[] intArray = {1,1,1,1,1,1,1,1,16};
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
+}
